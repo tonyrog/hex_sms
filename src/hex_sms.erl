92224e30dbfd831b0644ce8f18cc07c5d6bca77e
Tony Rogvall <tony@rogvall.se>
2014-02-25 17:15:51 +0100

    verify_event/init_event

--- src/hex_sms.erl
+++ src/hex_sms.erl
@@ -10,8 +10,11 @@
 
 -behaviour(hex_plugin).
 
--export([add_event/2, del_event/1, output/2]).
--export([validate_flags/2]).
+-export([validate_event/2, 
+	 init_event/2,
+	 add_event/2, 
+	 del_event/1, 
+	 output/2]).
 
 %%
 %%  add_event(Flags::[{atom(),term()}, Signal::signal()) ->    
@@ -39,43 +42,49 @@ output(Flags, _Env) ->
     gsms:send(Opts, Body).
 
 %%
-%% validate_flags(in | out, Flags::[{atom(),term()}])
+%% init_event(in | out, Flags::[{atom(),term()}])
 %%
-validate_flags(_Dir, []) ->
+init_event(_, _) ->
+    ok.
+
+%%
+%% validate_event(in | out, Flags::[{atom(),term()}])
+%%
+validate_event(_Dir, []) ->
     ok;
-validate_flags(Dir, [{Key,Value}|Kvs]) ->
+validate_event(Dir, [{Key,Value}|Kvs]) ->
     case Key of
 	smsc when is_list(Value) ->
 	    %% FIXME: validate msisdn
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	rp when is_boolean(Value) ->
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	udhi when is_boolean(Value) ->
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	udh when Value =:= [] ->
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	udh when is_list(Value) ->
 	    %% FIXME: validate udh codings
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	srr when is_boolean(Value) ->
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	mref when is_integer(Value), Value>=0, Value=< 255 ->
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	vpf when Value =:= none;
 		 Value =:= relative; 
 		 Value =:= enhanced; 
 		 Value =:= absolute ->
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	vp ->
 	    case Value of
 		none ->
-		    validate_flags(Dir, Kvs);
+		    validate_event(Dir, Kvs);
 		{relative, Seconds} when is_integer(Seconds), Seconds>=0 ->
-		    validate_flags(Dir, Kvs);
+		    validate_event(Dir, Kvs);
 		{absolute,DateTimeTz} ->
 		    case gsms_codec:is_valid_scts(DateTimeTz) of
 			true ->
-			    validate_flags(Dir, Kvs);
+			    validate_event(Dir, Kvs);
 			false ->
 			    {error, badarg}
 		    end;
@@ -84,31 +93,31 @@ validate_flags(Dir, [{Key,Value}|Kvs]) ->
 	    end;
 	addr when is_list(Value) ->
 	    %% FIXME: validate msisdn
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	pid when is_integer(Value), Value>=0, Value=<255 ->
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	dcs when is_integer(Value) ->
 	    _Dcs = gsms_codec:decode_dcs(Value),
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	type -> %% fixme test
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	class -> %% fixme test
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	alphabet -> %% fixme test
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	compression -> %% fixme test
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	store -> %% fixme test
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	wait_type -> %% fixme test
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	%% recognized options, but not for pdu
 	notify ->  %% fixme test?
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	ref ->  %% fixme test?
-	    validate_flags(Dir, Kvs);
+	    validate_event(Dir, Kvs);
 	_ ->
-	    lager:debug("validate_flags: unknown pdu option ~p", 
+	    lager:debug("validate_event: unknown pdu option ~p", 
 			[{Key,Value}]),
 	    {error, badarg}
     end.
